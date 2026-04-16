35445be79b3c10ff16fad8bfd9a3f8cac73945c0
AVICII <biswasagnibha97@gmail.com>
2026-04-16 17:29:11 +0530

    Removes application code and DevOps resources
    Deletes all source files, configuration, deployment scripts, infrastructure code, and documentation, effectively decommissioning the project and its associated automation. Cleans up both application and supporting DevOps tooling.

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Root route
-router.get('/', (req, res) => {
-  res.sendFile(require('path').join(__dirname, '../../public/index.html'));
-});
-
-module.exports = router;
